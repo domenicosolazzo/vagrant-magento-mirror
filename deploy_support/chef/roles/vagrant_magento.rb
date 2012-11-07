a794799e9c4dd993e27e037399eed6445275971e
Robert Coleman <github@robert.net.nz>
2012-11-07 19:42:02 +1300

    update database buffer sizes to avoid mysql crashes

--- deploy_support/chef/roles/vagrant_magento.rb
+++ deploy_support/chef/roles/vagrant_magento.rb
@@ -2,4 +2,11 @@ name "vagrant_magento"
 description "A LAMP enviroment for Magento."
 
 run_list "recipe[vagrant_magento]"
-override_attributes "mysql" => { "server_root_password" => "root", "allow_remote_root" => true }
+override_attributes "mysql" => { 
+  "server_root_password" => "root",
+  "allow_remote_root" => true,
+  "tunable" => {
+    "key_buffer" => "64M",
+    "innodb_buffer_pool_size" => "32M"
+  }
+}
